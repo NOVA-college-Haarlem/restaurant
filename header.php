9ee6f701d61cd7764ebb1ba2945c4c182db02368
David van Oudheusden <doudheusden@novacollege.nl>
2022-03-24 09:55:25 +0100

    added separate header

---
+++ header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <!--=============== basic  ===============-->
+    <meta charset="UTF-8">
+    <title>Lambert - Responsive Pub / Restaurant Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="robots" content="index, follow" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <!--=============== css  ===============-->
+    <link type="text/css" rel="stylesheet" href="css/reset.css">
+    <link type="text/css" rel="stylesheet" href="css/plugins.css">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <link type="text/css" rel="stylesheet" href="css/color.css">
+    <!--=============== favicons ===============-->
+    <link rel="shortcut icon" href="images/favicon.ico">
+</head>
+
+<body>
+
+    <div class="loader"><img src="images/loader.png" alt=""></div>
+    <!--================= main start ================-->
+    <div id="main">
+        <!--=============== header ===============-->
+        <header>
+            <div class="header-inner">
+                <div class="container">
+                    <!--navigation social links-->
+                    <div class="nav-social">
+                        <ul>
+                            <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fa fa-tumblr"></i></a></li>
+                        </ul>
+                    </div>
+                    <!--logo-->
+                    <div class="logo-holder">
+                        <a href="index.html">
+                            <img src="images/logo.png" class="respimg logo-vis" alt="">
+                            <img src="images/logo2.png" class="respimg logo-notvis" alt="">
+                        </a>
+                    </div>
+                    <!--Navigation -->
+                    <div class="subnav">
+                        <a href="cart.html"><i class="fa fa-shopping-cart"></i>( <span>2</span> )</a>
+                    </div>
+                    <!--Navigation -->
+                    <?php include 'navigation.php'; ?>
+                </div>
+            </div>
+        </header>
+        <!--header end-->
