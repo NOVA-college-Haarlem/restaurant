0d65ba5ddbbfbddbdbd62a44bafcceed19dc488e
David van Oudheusden <doudheusden@novacollege.nl>
2022-03-24 09:51:27 +0100

    git init

---
+++ navigation.php
@@ -0,0 +1,13 @@
+<div class="nav-holder">
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="menu.html">Menu</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="reservation.html">Reservation</a></li>
+            <li><a href="shop.html">Shop</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="blog.html" class="act-link">Journal</a></li>
+        </ul>
+    </nav>
+</div>
