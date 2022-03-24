a1f740dfdce3f3a91f2dbcca27ff916924152035
David van Oudheusden <doudheusden@novacollege.nl>
2022-03-24 12:39:18 +0100

    added active menu

--- navigation.php
+++ navigation.php
@@ -1,13 +1,16 @@
+<?php
+$page = basename($_SERVER["SCRIPT_FILENAME"], '.php');
+?>
 <div class="nav-holder">
     <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="menu.php">Menu</a></li>
-            <li><a href="gallery.php">Gallery</a></li>
-            <li><a href="reservation.php">Reservation</a></li>
-            <li><a href="shop.php">Shop</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="blog.php" class="act-link">Journal</a></li>
+            <li><a href="index.php" class="<?php echo ($page == "index" ? 'act-link' : '') ?>">Home</a></li>
+            <li><a href="menu.php" class="<?php echo ($page == "menu" ? 'act-link' : '') ?>">Menu</a></li>
+            <li><a href="gallery.php" class="<?php echo ($page == "gallery" ? 'act-link' : '') ?>">Gallery</a></li>
+            <li><a href="reservation.php" class="<?php echo ($page == "reservation" ? 'act-link' : '') ?>">Reservation</a></li>
+            <li><a href="shop.php" class="<?php echo ($page == "shop" ? 'act-link' : '') ?>">Shop</a></li>
+            <li><a href="contact.php" class="<?php echo ($page == "contact" ? 'act-link' : '') ?>">Contact</a></li>
+            <li><a href="blog.php" class="<?php echo ($page == "blog" ? 'act-link' : '') ?>">Journal</a></li>
         </ul>
     </nav>
 </div>
